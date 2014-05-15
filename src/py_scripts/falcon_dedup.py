d1264700173383894454470812b3e74366b1b8e8
pb-jchin <jchin@pacificbiosciences.com>
2014-05-14 18:27:54 -0700

    change the output from stdout to a defult files. TODO, move this the falcon_pp (falcon post processing) eventually

--- src/py_scripts/falcon_dedup.py
+++ src/py_scripts/falcon_dedup.py
@@ -81,38 +81,39 @@ with open("primary_tigs_node_pos_c") as f:
         p_tig_to_node_pos[l[0]].append( (l[1], int(l[2])))
 
 duplicate_a_tigs = []
-for p_tig_id in p_tig_path:
-    main_path = p_tig_path[p_tig_id]
-    main_path_nodes = set(main_path[:])
-    p_tig_seq = p_tig_seqs[p_tig_id]
-    a_node = []
-    a_node_range = []
-    a_node_range_map = {}
-    node_to_pos = dict( p_tig_to_node_pos[p_tig_id] )
-    for id_ in a_tig_path:
-        if id_[:4] != p_tig_id[:4]:
-            continue
-        if id_.split("-")[1] == "0000":
-            continue
-        
-        a_path = a_tig_path[id_]
-        if a_path[0] in main_path_nodes and a_path[-1] in main_path_nodes:
-            #print p_tig_id, id_, a_path[0], a_path[-1]
-            s, e = node_to_pos[a_path[0]], node_to_pos[a_path[-1]]
-            p_seq = p_tig_seq[s:e]
-            a_seq = a_tig_seqs[id_] 
-            seq_match = get_matches(p_seq, a_seq)
-            if len(seq_match) > 1:
-                print ">"+id_
-                print a_seq
+with open("a_nodup.fa","w") as out_f:
+    for p_tig_id in p_tig_path:
+        main_path = p_tig_path[p_tig_id]
+        main_path_nodes = set(main_path[:])
+        p_tig_seq = p_tig_seqs[p_tig_id]
+        a_node = []
+        a_node_range = []
+        a_node_range_map = {}
+        node_to_pos = dict( p_tig_to_node_pos[p_tig_id] )
+        for id_ in a_tig_path:
+            if id_[:4] != p_tig_id[:4]:
                 continue
-            try:
-                r_s, r_e = seq_match[0][0][0][0], seq_match[0][-1][0][1]
-            except:
-                print "XXX", seq_match
-            if 1.0* (r_e - r_s) / (e - s) > 98:
-                print ">"+id_
-                print a_seq
+            if id_.split("-")[1] == "0000":
                 continue
-            duplicate_a_tigs.append(id_)
+            
+            a_path = a_tig_path[id_]
+            if a_path[0] in main_path_nodes and a_path[-1] in main_path_nodes:
+                #print p_tig_id, id_, a_path[0], a_path[-1]
+                s, e = node_to_pos[a_path[0]], node_to_pos[a_path[-1]]
+                p_seq = p_tig_seq[s:e]
+                a_seq = a_tig_seqs[id_] 
+                seq_match = get_matches(p_seq, a_seq)
+                if len(seq_match) > 1:
+                    print >>out_f, ">"+id_
+                    print >>out_f,  a_seq
+                    continue
+                try:
+                    r_s, r_e = seq_match[0][0][0][0], seq_match[0][-1][0][1]
+                except:
+                    print "XXX", seq_match
+                if 1.0* (r_e - r_s) / (e - s) > 98:
+                    print >>out_f, ">"+id_
+                    print >>out_f, a_seq
+                    continue
+                duplicate_a_tigs.append(id_)
 
