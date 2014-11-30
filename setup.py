19fb868b9b2fe53dc72d0df6bc1fcc81d76fa300
pb-jchin <jchin@pacificbiosciences.com>
2014-11-29 22:17:31 -0800

    setup for v2, remove the dependence for pbcore for v2
    pbcore is still needed for v1 but v1 is going to be obsoleted soon.

--- setup.py
+++ setup.py
@@ -4,6 +4,27 @@ from setuptools import setup
 
 from distutils.core import Extension
 
+import glob
+
+scripts = ["src/py_scripts/falcon_asm.py", 
+         "src/py_scripts/falcon_overlap.py",
+         "src/py_scripts/falcon_overlap2.py",
+         "src/py_scripts/falcon_qrm.py",
+         "src/py_scripts/falcon_fixasm.py",
+         "src/py_scripts/falcon_dedup.py",
+         "src/py_scripts/falcon_sense.py",
+         "src/py_scripts/falcon_ucns_data.py",
+         "src/py_scripts/falcon_utgcns.py",
+         "src/py_scripts/get_rdata.py",
+         "src/py_scripts/remove_dup_ctg.py"]
+
+scripts_v2 = glob.glob("src/py_scripts_v2/*.py")
+
+#install_requires=[ "pbcore >= 0.6.3", "networkx >= 1.7" ]
+install_requires=[ "networkx >= 1.7" ]
+
+scripts.extend( scripts_v2 )
+
 setup(name='falcon_kit',
       version='0.1.3',
       description='a small toolkit for DNA seqeucne alignment, overlapping, and assembly',
@@ -18,19 +39,8 @@ setup(name='falcon_kit',
                    Extension('falcon_kit.falcon', ['src/c/DW_banded.c', 'src/c/kmer_lookup.c', 'src/c/falcon.c'],
                    extra_link_args=["-fPIC",  "-O3"]),
                    ],
-      scripts = ["src/py_scripts/falcon_asm.py", 
-                 "src/py_scripts/falcon_overlap.py",
-                 "src/py_scripts/falcon_overlap2.py",
-                 "src/py_scripts/falcon_qrm.py",
-                 "src/py_scripts/falcon_fixasm.py",
-                 "src/py_scripts/falcon_dedup.py",
-                 "src/py_scripts/falcon_sense.py",
-                 "src/py_scripts/falcon_ucns_data.py",
-                 "src/py_scripts/falcon_utgcns.py",
-                 "src/py_scripts/falcon_sense.py",
-                 "src/py_scripts/get_rdata.py",
-                 "src/py_scripts/remove_dup_ctg.py"],
+      scripts = scripts,
       zip_safe = False,
-      install_requires=[ "pbcore >= 0.6.3", "networkx >= 1.7" ]
+      install_requires=install_requires
      )
 
