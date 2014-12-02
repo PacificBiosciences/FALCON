4e672357caa98c4ba96e6387ea37036f776ae225
pb-jchin <jchin@pacificbiosciences.com>
2014-12-02 14:53:53 -0800

    Add more configuration file examples, a build script a script to run a simple assembly with E. coli data

---
+++ examples/build_env.sh
@@ -0,0 +1,22 @@
+virtualenv --no-site-packages  --always-copy   $PWD/fc_env
+. $PWD/fc_env/bin/activate
+git clone https://github.com/cschin/pypeFLOW 
+cd pypeFLOW
+python setup.py install
+
+cd ..
+git clone https://github.com/PacificBiosciences/FALCON.git
+cd FALCON
+python setup.py install
+
+cd ..
+git clone https://github.com/cschin/DAZZ_DB.git
+cd DAZZ_DB/
+make
+cp DBrm DBshow DBsplit DBstats fasta2DB ../fc_env/bin/ 
+
+cd ..
+git clone https://github.com/cschin/DALIGNER.git
+cd DALIGNER
+make
+cp daligner daligner_p DB2Falcon HPCdaligner LA4Falcon LAmerge LAsort  ../fc_env/bin
---
+++ examples/run_ecoli_test.sh
@@ -0,0 +1,11 @@
+mkdir ecoli_test/
+cd ecoli_test/
+mkdir data
+cd data
+wget https://www.dropbox.com/s/tb78i5i3nrvm6rg/m140913_050931_42139_c100713652400000001823152404301535_s1_p0.1.subreads.fasta
+wget https://www.dropbox.com/s/v6wwpn40gedj470/m140913_050931_42139_c100713652400000001823152404301535_s1_p0.2.subreads.fasta
+wget https://www.dropbox.com/s/j61j2cvdxn4dx4g/m140913_050931_42139_c100713652400000001823152404301535_s1_p0.3.subreads.fasta
+cd ..
+find $PWD/data -name "*.fasta" > input.fofn
+cp ../FALCON/examples/fc_run_ecoli_2.cfg  .
+fc_run.py fc_run_ecoli_2.cfg 
