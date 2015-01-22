944b5b6a29557d7228caab452a5f9bf5a4bdb2ac
lhon <lhon@pacificbiosciences.com>
2015-01-22 11:45:52 -0800

    allow job_type=local to work

--- src/py_scripts/fc_run.py
+++ src/py_scripts/fc_run.py
@@ -180,7 +180,7 @@ def run_daligner(self):
                 "cwd": cwd,
                 "sge_option": sge_option_da,
                 "script_fn": script_fn }
-    run_script(job_data, job_type = "SGE")
+    run_script(job_data, job_type = config["job_type"])
     wait_for_file( fn( self.job_done ), task=self, job_name=job_name )
 
 def run_merge_task(self):
@@ -213,7 +213,7 @@ def run_merge_task(self):
                 "cwd": cwd,
                 "sge_option": sge_option_la,
                 "script_fn": script_fn }
-    run_script(job_data, job_type = "SGE")
+    run_script(job_data, job_type = config["job_type"])
     wait_for_file( fn( self.job_done ), task=self, job_name=job_name )
 
 def run_consensus_task(self):
@@ -251,7 +251,7 @@ def run_consensus_task(self):
                 "cwd": cwd,
                 "sge_option": sge_option_cns,
                 "script_fn": script_fn }
-    run_script(job_data, job_type = "SGE")
+    run_script(job_data, job_type = config["job_type"])
     wait_for_file( os.path.join(cwd,"c_%05d_done" % job_id) , task=self, job_name=job_name )
 
 
@@ -731,7 +731,7 @@ if __name__ == '__main__':
                     "cwd": wd,
                     "sge_option": config["sge_option_fc"],
                     "script_fn": script_fn }
-        run_script(job_data, job_type = "SGE")
+        run_script(job_data, job_type = config["job_type"])
         wait_for_file( fn(self.falcon_asm_done), task=self, job_name=job_name )
     
     wf.addTask( run_falcon_asm_task )
