35b86a239d6ad65f9766bf8a12188179a9dc80de
Gregory Concepcion <gconcepcion@hyperion.nanofluidics.com>
2016-11-02 15:35:08 -0700

    setup tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# FALCON documentation build configuration file, created by
+# FALCON build configuration file, created by
 # sphinx-quickstart on Wed Nov  2 13:50:34 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -133,7 +133,7 @@ html_theme = 'alabaster'
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = u'FALCON Documentation v0.5'
+# html_title = u'FALCON v0.5'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
@@ -233,7 +233,7 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'FALCONDocumentationdoc'
+htmlhelp_basename = 'FALCONdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -259,7 +259,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'FALCON.tex', u'FALCON Documentation',
+    (master_doc, 'FALCON.tex', u'FALCON',
      u'Jason Chin, Greg Concepcion, Sarah Kingan', 'manual'),
 ]
 
@@ -301,7 +301,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'falcon', u'FALCON Documentation',
+    (master_doc, 'falcon', u'FALCON',
      [author], 1)
 ]
 
@@ -316,7 +316,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'FALCON', u'FALCON  Documentation',
+    (master_doc, 'FALCON', u'FALCON',
      author, 'FALCON', 'One line description of project.',
      'Miscellaneous'),
 ]
