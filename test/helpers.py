457b383aa3d6de51845e7c0a1705ceeebf95d434
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-04-19 07:24:03 -0700

    More coverage

--- test/helpers.py
+++ test/helpers.py
@@ -1,4 +1,5 @@
 from nose.tools import assert_equal, assert_raises, eq_
+import os.path
 
 def equal_list(a, b):
     eq_(set(a) ^ set(b), set())
@@ -12,3 +13,6 @@ def equal_multiline(a, b):
     alines = a.splitlines()
     blines = b.splitlines()
     equal_list(alines, blines)
+
+def get_test_data_dir():
+    return os.path.join(os.path.dirname(__file__), '..', 'test_data')
