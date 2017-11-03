1481e2eeaf2119143ca4f26cf2349d1bc76d3813
Greg Concepcion <gconcepcion@pacificbiosciences.com>
2017-11-03 14:31:27 -0700

    simpler install script - just untarring binaries and building MUMmer

--- docs/scripts/install_unzip.sh
+++ docs/scripts/install_unzip.sh
@@ -7,7 +7,6 @@
 ##available in your $PATH
 
 #source /mnt/software/Modules/current/init/bash
-#module load gcc/4.9.2
 #module load python/2.7.9
 #module load virtualenv/13.0.1
 #module load git
@@ -29,9 +28,8 @@ FALCON_REPO="https://github.com/PacificBiosciences/FALCON-integrate.git"
 DL_CLOUD="https://downloads.pacbcloud.com/public/falcon/"
 
 ###Test whether you need ucs4 or ucs2 tarball with this command:
-#  $ python2.7 -c 'import sysconfig,pprint; pprint.pprint(sysconfig.get_config_vars()["Py_UNICODE_SIZE"])'
-UNZIP_TARBALL="falcon-2017.06.28-18.01-py2.7-ucs4.tar.gz"
-#UNZIP_TARBALL="falcon-2017.06.28-18.01-py2.7-ucs2.tar.gz"
+UCS_VER=$(python2.7 -c 'import sysconfig,pprint; pprint.pprint(sysconfig.get_config_vars()["Py_UNICODE_SIZE"])')
+UNZIP_TARBALL="falcon-2017.11.02-16.04-py2.7-ucs${UCS_VER}.tar.gz"
 
 ###Cleanup
 if [ ! -d ${ROOT} ]; then
@@ -59,40 +57,20 @@ if [ -L "${VENV_BASE}" ]; then
     unlink ${VENV_BASE}
 fi
 
-
+###Make python virtualenv
 virtualenv --no-site-packages ${VENV}
 echo "export LD_LIBRARY_PATH=${VENV}/lib:\${LD_LIBRARY_PATH}" >>${VENV}/bin/activate
-
 source ${VENV}/bin/activate
 
-cd $SRC
-git clone ${FALCON_REPO}
-
-###install falcon
-
-cd ${FALCON_PATH}
-git checkout master
-git submodule update --init
-sed -i "s|^FALCON_WORKSPACE.*|FALCON_WORKSPACE=${FALCON_PATH}|g" default-env.sh
-sed -i "s|^PYTHONUSERBASE.*|PYTHONUSERBASE=${VENV}|g" default-env.sh
-make init
-source env.sh
-
-make config-standard
-make -j all
-make install
-#make test
-
-###install unzip
+###Install FALCON and FALCON_unzip
 
 cd ${SRC}
 curl -O ${DL_CLOUD}/${UNZIP_TARBALL}
-tar zxvf ${UNZIP_TARBALL}  -C ${VENV}
+tar zxvf ${UNZIP_TARBALL} -C ${VENV}
 
 ln -s ${VENV} ${VENV_BASE}
 
-
-###install MUMmer 3.23 - for FALCON_unzip
+###Install MUMmer 3.23 - for FALCON_unzip
 
 MUMMER_323='https://downloads.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz'
 cd ${SRC}
@@ -101,14 +79,13 @@ tar zxvf MUMmer3.23.tar.gz
 cd MUMmer3.23
 make install
 cp nucmer ${VENV}/bin
-
+cp show-coords ${VENV}/bin
 
 ###Test falcon_unzip pipeline
-
-cd ${FALCON_PATH}/FALCON-examples
-../git-sym/git-sym update run/greg200k-sv2
-cd run/greg200k-sv2
-fc_run fc_run.cfg
+cd ${SRC} && git clone ${FALCON_REPO}
+cd ${FALCON_PATH} && git submodule update --init
+cd ${FALCON_PATH}/FALCON-examples && ../git-sym/git-sym update run/greg200k-sv2
+cd run/greg200k-sv2 && fc_run fc_run.cfg
 
 sed -i "s|^smrt_bin=.*|smrt_bin=${VENV}/bin|g" fc_unzip.cfg
 fc_unzip.py fc_unzip.cfg
