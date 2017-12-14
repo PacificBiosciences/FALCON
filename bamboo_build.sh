4b0a09ddaa3044a7367cd62f8ae3c0a6f745995d
David Seifert <dseifert@pacificbiosciences.com>
2017-12-14 22:16:28 +0100

    Use default version of individual modules
    * Makes future dependency management easier and less cumbersome

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -2,10 +2,10 @@
 
 type module >& /dev/null || . /mnt/software/Modules/current/init/bash
 module unload git gcc ccache
-module load git/2.8.3
-module load gcc/6.4.0
-module load ccache/3.2.3
-module load python/2.7.13-UCS4
+module load git
+module load gcc
+module load ccache
+module load python/2
 #module load make
 
 set -vx
