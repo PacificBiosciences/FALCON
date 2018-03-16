ec8ff5d41756c502abdfdc475188eda1eccf8878
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-03-16 05:16:38 -0700

    Get abspaths for inputs
    The parallel steps write relative paths, since they might run in /tmp.
    The generic gatherer absolutizes those. However, if pbsmrtpipe
    is run without chunking, then that gatherer is not run.
    So we need to absolutize explicitly in the pseudo-gatherers.
    (Maybe there is a better place to do this?)

--- falcon_kit/mains/las_merge_gather.py
+++ falcon_kit/mains/las_merge_gather.py
@@ -16,11 +16,16 @@ LOG = logging.getLogger()
 
 def run(gathered_fn, las_fn, p_id2las_fn):
     gathered = io.deserialize(gathered_fn)
+    d = os.path.abspath(os.path.realpath(os.path.dirname(gathered_fn)))
+    def abspath(fn):
+        if os.path.isabs(fn):
+            return fn # I expect this never to happen though.
+        return os.path.join(d, fn)
     p_id2las = dict()
     for desc in gathered:
-        p_id_fn = desc['p_id']
+        p_id_fn = abspath(desc['p_id'])
         p_id = int(open(p_id_fn).read()) #io.deserialize(p_id_fn) # someday? requires file-extension
-        p_id2las[p_id] = desc['merged_las']
+        p_id2las[p_id] = abspath(desc['merged_las'])
     io.serialize(p_id2las_fn, p_id2las)
     io.serialize(las_fn, list(p_id2las.values()))
 
