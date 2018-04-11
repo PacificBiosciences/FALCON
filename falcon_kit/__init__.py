a6195b12cb13c352353cd9484df451209893d3cb
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-04-11 16:55:55 -0700

    Always show SHA1 on start

--- falcon_kit/__init__.py
+++ falcon_kit/__init__.py
@@ -1,3 +1,9 @@
 from __future__ import absolute_import
 
 from .falcon_kit import *
+
+try:
+    import sys, pkg_resources
+    sys.stderr.write('{}\n'.format(pkg_resources.get_distribution('falcon-kit')))
+except Exception:
+    pass
